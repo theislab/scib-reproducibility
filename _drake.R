e455b4fed4eafdfbd8c4d0785faaed4cfa26ea79
Luke Zappia <lazappi@users.noreply.github.com>
2020-09-30 13:48:12 +0200

    Add dataset to drake

--- _drake.R
+++ _drake.R
@@ -6,10 +6,19 @@ library("drake")
 library("here")
 library("tidyverse")
 
+library("htmltools")
+
 #==============================================================================#
 # ---- FUNCTIONS ----
 #==============================================================================#
 
+# See https://github.com/rstudio/gt/issues/297#issuecomment-497778735
+callr_render <- function(input, output_file) {
+    callr::r(
+        function(...) rmarkdown::render(...),
+        args = list(input = input, output_file = output_file)
+    )
+}
 
 #==============================================================================#
 # ---- PLAN ----
@@ -18,6 +27,13 @@ library("tidyverse")
 plan <- drake_plan(
     configs = list(
         site = read_lines(here(file_in("pages/_site.yml")))
+    ),
+    dataset = target(
+        callr_render(
+            here(knitr_in("pages/dataset.Rmd")),
+            here(file_out("docs/dataset.html"))
+        ),
+        trigger = trigger(change = configs)
     )
 )
 
